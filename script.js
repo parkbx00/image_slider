a04b56abcb94755d96919e5d0f953a43d47b0957
parkbx00 <parkbx0900@gmail.com>
2021-06-18 18:11:55 -0400

    JS & CSS: Complete image slide function. Add hover effects on icons.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//1. Get all the DOM elements
 const dropboxContainer = document.querySelector(".container-dropbox");
 const dropbox = document.querySelector(".dropbox");
 const play = document.querySelector(".fa-play");
@@ -8,54 +7,53 @@ const btnBrowse = document.querySelector(".dropbox__btn-browse");
 const inputFile = document.querySelector(".dropbox__file-loader");
 const dropboxText = document.querySelector(".dropbox p");
 const sliderContainer = document.querySelector(".container-slider");
-const slides = document.querySelector(".slides");
+const slidesContainer = document.querySelector(".slides");
+let slides = document.querySelectorAll("slide");
+const instructionContainer = document.querySelector(".container-instruction");
 
-// let file;
 let images = [];
 const validExtension = ["image/png", "image/jpeg", "image/jpg"];
 
-//2. Add event listener for dragOver
+// Add event listener for dragOver
 dropbox.addEventListener("dragover", (e) => {
     e.preventDefault();
     dropbox.classList.add("active");
     dropboxText.textContent = "Release to upload the image.";
 });
 
-//3. Add event listener for dragLeave
+// Add event listener for dragLeave
 dropbox.addEventListener("dragleave", () => {
     dropbox.classList.remove("active");
     dropboxText.textContent = "Drag & Drop Images to Upload.";
 });
 
-// 4. Add event listener for drop
+// Add event listener for drop
 dropbox.addEventListener("drop", (e) => {
     e.preventDefault();
 
     images = Array.from(e.dataTransfer.files);
-    console.log(images); // Remove after
-
     isAllExtensionValid(images);
 
     images = e.dataTransfer.files;
-    console.log(images); // Remove after
 
     dropbox.classList.remove("active");
-    dropboxText.textContent = "Drag & Drop New Images to Upload.";
+    dropboxText.textContent = "Click play button to start or add more images.";
 
     for (let i = 0; i < images.length; i++) {
         loadImage(images[i]);
     }
+
+    slides = document.querySelectorAll(".slide");
+    slides[0].classList.add("visible");
 });
 
 // Open up file input when Browse File button is clicked.
-btnBrowse.addEventListener("click", (e) => {
+btnBrowse.addEventListener("click", () => {
     inputFile.click();
 });
 
-inputFile.addEventListener("change", (e) => {
-    images = [];
-    slides.innerHTML = "";
-
+//
+inputFile.addEventListener("change", () => {
     images = Array.from(inputFile.files);
     isAllExtensionValid(images);
 
@@ -65,27 +63,50 @@ inputFile.addEventListener("change", (e) => {
         loadImage(images[i]);
     }
 
-    dropboxText.textContent = "Click play button to start!";
+    slides = document.querySelectorAll(".slide");
+    slides[0].classList.add("visible");
+
+    dropboxText.textContent = "Click play button to start or add more images.";
 });
 
 // Start slide show when play icon is clicked.
+const intervalTime = 5000;
+let slideshowInterval;
+
 play.addEventListener("click", () => {
+    if (slides.length === 0) {
+        alert("Please upload images first.");
+        return;
+    }
+
     // Hide container-dropbox
     dropboxContainer.style.display = "none";
     sliderContainer.style.display = "block";
+    instructionContainer.style.display = "none";
+
+    play.classList.add("active");
+    pause.classList.remove("active");
 
     // Slide images
+    slideshowInterval = setInterval(nextSlide, intervalTime);
 });
 
 // Pause slide show when pause icon is clicked.
 pause.addEventListener("click", () => {
-    // Pause slide show
+    if (slides.length === 0) {
+        alert("Please upload images first.");
+        return;
+    }
+
+    play.classList.remove("active");
+    pause.classList.add("active");
+
+    clearInterval(slideshowInterval);
 });
 
 // Resetting when trashcan icocn is clicked.
 reset.addEventListener("click", () => {
     location.reload();
-    // Also should reset local storage
 });
 
 // Function to verify image extensions.
@@ -98,7 +119,6 @@ function isAllExtensionValid(images) {
         alert("Please upload images with .png, .jpeg, or .jpg extension.");
         dropbox.classList.remove("active");
         images = [];
-        console.log(images);
         return;
     }
 }
@@ -107,15 +127,25 @@ function isAllExtensionValid(images) {
 function loadImage(image) {
     let imgContainer = document.createElement("div");
     imgContainer.classList.add("slide");
-    slides.appendChild(imgContainer);
-
-    let slideImg = document.createElement("img");
-    imgContainer.appendChild(slideImg);
 
     let fileReader = new FileReader();
     fileReader.onload = () => {
-        slideImg.src = fileReader.result;
+        imgContainer.setAttribute("style", `background: url(${fileReader.result}) no-repeat center/cover;`);
     };
+    slidesContainer.appendChild(imgContainer);
 
     fileReader.readAsDataURL(image);
+}
+
+// Function to initiate slide show
+function nextSlide() {
+    const currentSlide = document.querySelector(".visible");
+
+    currentSlide.classList.remove("visible");
+
+    if (currentSlide.nextElementSibling) {
+        currentSlide.nextElementSibling.classList.add("visible");
+    } else {
+        slides[0].classList.add("visible");
+    }
 }
